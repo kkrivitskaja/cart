c85fcbdfb9c123c38bc9330cabbfe7941599b5e7
kkrivitskaja <pratugames@gmail.com>
2022-07-01 22:08:59 +0300

    feat: moved types into separate file to increase their reusability

---
+++ src/types/types.tsx
@@ -0,0 +1,52 @@
+export type AttributeItemType = {
+    displayValue: string,
+    value: string,
+    id: string,
+}
+
+export type AttributeType = {
+    id: string,
+    items: AttributeItemType[],
+    name: string,
+    type: string,
+}
+
+export type CurrencyType = {
+    label: string,
+    symbol: string,
+}
+
+export type PriceType = {
+    currency: CurrencyType,
+    amount: number,
+}
+
+export type productType = {
+    id: string,
+    productId: string,
+    name: string,
+    inStock: boolean,
+    gallery: string[],
+    attributes: AttributeType[],
+    brand: string,
+    prices: PriceType[],
+    selectedAttribute: SelectedAttributeType,
+    amount: number,
+    productUrl: string,
+}
+
+export type SelectedAttributeType = {
+    id: string,
+    item: AttributeItemType,
+    name: string,
+    type: string,
+}
+
+export type storageVarType = {
+    currentCurrency: CurrencyType,
+    productsInCart: productType[],
+    modalMessage: React.ReactNode,
+    isModalWindow: boolean,
+    isCurrencyDropdown: boolean,
+    isCartOverlay: boolean,
+}
