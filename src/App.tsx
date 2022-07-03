1dc0ede812a574e1ad3d5831e96bf318250d1cb4
kkrivitskaja <pratugames@gmail.com>
2022-07-03 18:50:04 +0300

    feat: added PageNotFound for user redirect if no path matched the existing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+import { Home } from './pages/Home'
+import { Store } from './pages/Store'
+import { About } from './pages/About'
+import { Navbar } from './components/Navbar/Navbar'
+import PageNotFound from './pages/PageNotFound/PageNotFound'
+
+
 function App() {
-  
+
   return (
     <div className="App">
-     <h3>Shopping cart</h3>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route path="*" element={<Navigate replace to="/404" />} />
+          <Route path="/404" element={<PageNotFound />} />
+        </Route>
+      </Routes>
+
     </div>
   )
 }
--- src/pages/Store.tsx
+++
@@ -1,3 +0,0 @@
-export function Store() {
-    return <h2>Store</h2>
-}
