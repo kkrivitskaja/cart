5311c3a362b9c089619c7bbdb1c09003f9e4da90
kkrivitskaja <pratugames@gmail.com>
2022-07-03 18:41:15 +0300

    fix: path to the icon

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,17 +1,20 @@
 import { Component } from 'react';
 
+import { ReactComponent as Error } from '../../assets/404-error-icon.svg';
 import styles from './PageNotFound.module.scss';
 
-
+const steps=['Shipping', 'Review&Payments', 'Succes']
 class PageNotFound extends Component {
     render() {
         return (
             <>
                 <div className={styles['message']}>
+                    <Error/>
                     <span className={styles['message-title']}>OOPS! PAGE NOT BE FOUND </span>
                     Sorry but the page you are looking for does not exist, have been removed. name
                     changed or is temporarily unavailable
                 </div>
+                
             </>
         );
     }
